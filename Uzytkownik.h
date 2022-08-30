4918ba5441ecc762e472fc4d2c75b8b955c81df4
Lukasz Malinowski <lukasz.malinowski.programista@gmail.com>
2022-08-31 01:14:19 +0200

    Add registerd used to vector

--- Uzytkownik.h
+++ Uzytkownik.h
@@ -4,9 +4,16 @@ using namespace std;
 
 class Uzytkownik
 {
-
-
     int id;
     string login;
     string haslo;
+
+public:
+    void ustawId(int noweId);
+    void ustawLogin(string nowyLogin);
+    void ustawHaslo(string niweHaslo);
+
+    int pobierzId();
+    string pobierzLogin();
+    string pobierzHaslo();
 };
