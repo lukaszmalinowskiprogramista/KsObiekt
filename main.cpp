14a6a6ffe07ed7884aaa35b86585a6cc69d33a2c
Lukasz Malinowski <lukasz.malinowski.programista@gmail.com>
2022-09-07 22:06:39 +0200

    Added setup filename at begining

--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@ using namespace std;
 
 int main()
 {
-    KsiazkaAdresowa ksiazkaAdresowa;
-    //ksiazkaAdresowa.wczytajUzytkownikowZPliku();
-    //ksiazkaAdresowa.rejestracjaUzytkownika();
-    //ksiazkaAdresowa.rejestracjaUzytkownika();
+    KsiazkaAdresowa ksiazkaAdresowa("Uzytkownicy.txt");
+
+    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.rejestracjaUzytkownika();
     ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
     return 0;
 }
